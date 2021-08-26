092be38de84dae3c989f32ff01076aeacdc7ccd4
Mika Geebelen <Geebelen.Mika@gmail.com>
2021-08-26 15:00:26 +0200

    Fixed null AI glitch and added a bit more area to the test map

--- Source/Prototype/Private/Enemies/EnemyManager.cpp
+++ Source/Prototype/Private/Enemies/EnemyManager.cpp
@@ -12,8 +12,14 @@
 AActor* AEnemyManager::SpawnWanderingEnemy(FVector spawnLoc, float range, float wanderRange)
 {
 	ABaseEnemyController* pController = Cast<ABaseEnemyController>(GetWorld()->SpawnActor(m_WanderController));
-	ABaseEnemy* pPawn = Cast<ABaseEnemy>(GetWorld()->SpawnActor(m_WanderBase));
-	pPawn->SetActorLocation(spawnLoc);
+
+	const FVector loc = spawnLoc;
+	const FRotator rot{};
+
+	FActorSpawnParameters params{};
+	params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+	
+	ABaseEnemy* pPawn = Cast<ABaseEnemy>(GetWorld()->SpawnActor(m_WanderBase, &loc, &rot,params));
 
 	pController->m_WanderRange = wanderRange;
 	pController->m_Range = range;
@@ -28,9 +34,14 @@ AActor* AEnemyManager::SpawnWanderingEnemy(FVector spawnLoc, float range, float 
 AActor* AEnemyManager::SpawnPatrollingEnemy(FVector spawnLoc, TArray<AActor*> patrolArray, float range)
 {
 	APatrollingController* pController = Cast<APatrollingController>(GetWorld()->SpawnActor(m_PatrolController));
-	auto magic = GetWorld()->SpawnActor(m_PatrolBase);
-	ABaseEnemy* pPawn = Cast<ABaseEnemy>(magic);
-	pPawn->SetActorLocation(spawnLoc);
+
+	const FVector loc = spawnLoc;
+	const FRotator rot{};
+
+	FActorSpawnParameters params{};
+	params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+	
+	ABaseEnemy* pPawn = Cast<ABaseEnemy>(GetWorld()->SpawnActor(m_PatrolBase, &loc, &rot, params));
 
 	pController->m_Waypoints = patrolArray;
 	pController->m_Range = range;
